941e9ac491a1b6a34a68400dd22294055b5b90c6
Silvio Goncalves <silvio.goncalves@aluno.uece.br>
2024-01-30 18:37:20 -0300

    Att

---
+++ Estudo/Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudo.Fundamentos
+{
+    internal class PrimeiroPrograma
+    {
+        static void Main()
+        {
+            System.Console.Write("Primeiro ");
+            Console.WriteLine("Programa");
+            Console.WriteLine("Terminou!");
+        }
+    }
+}
