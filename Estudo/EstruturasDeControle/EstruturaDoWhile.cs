97985f694321ab2a19e4748f341a8815d2a259af
Silvio Gonçalves <silvio.goncalves@aluno.uece.br>
2024-08-01 17:10:44 -0300

    Att

---
+++ Estudo/EstruturasDeControle/EstruturaDoWhile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.EstruturasDeControle
+{
+    internal class EstruturaDoWhile
+    {
+        public static void Executar()
+        {
+            string entrada;
+            //No 'do' irá ser lido ao menos 1 vez, independente da condição do while
+            do
+            {
+                Console.WriteLine("Qual o seu nome: ");
+                entrada = Console.ReadLine();
+
+                Console.WriteLine("Seja bem vindo {0}", entrada);
+                Console.WriteLine("Deseja continuar? (S/N)");
+                entrada = Console.ReadLine();
+            } while (entrada.ToLower() == "s");
+        }
+    }
+}
---
+++ Estudo/EstruturasDeControle/EstruturaWhile.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.EstruturasDeControle
+{
+    internal class EstruturaWhile
+    {
+        public static void Executar()
+        {
+            int palpite = 0;
+            Random random = new Random();
+
+            int numeroSecreto = random.Next(1, 16);//Vai de 1 a 15, pois o 16 não entra, funcionando igual a indice
+            bool numeroEncontrado = false;
+            int tentativasRestantes = 5;
+            int tentativas = 0;
+
+            while (tentativasRestantes > 0 && !numeroEncontrado) {
+                Console.WriteLine("Insira seu palpite: ");
+                string entrada = Console.ReadLine();
+                int.TryParse(entrada, out palpite);
+
+                tentativas++;
+                tentativasRestantes--;
+
+                if (numeroSecreto == palpite)
+                {
+                    numeroEncontrado = true;
+                    var corAnterior = Console.BackgroundColor;//Armazena a cor original do terminal
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Número encontrado em {0} tentativas", tentativas);
+                    Console.BackgroundColor = corAnterior; //Volta a cor normal. Se não fizer, toda saida sairá em verde
+
+                } else if (palpite > numeroSecreto)
+                {
+                    Console.WriteLine("Menor... Tente novamente");
+                    Console.WriteLine("Tentativas restantes {0}", tentativasRestantes);
+
+                }else
+                {
+                    Console.WriteLine("Maior... Tente novamente");
+                    Console.WriteLine("Número de tentativas {0}", tentativasRestantes);
+                }
+            }
+        }
+    }
+}
